1431effb0a3b7be6491f7b7648aa8a3f3a6171e1
Junseo Park <jspark2730@gmail.com>
2022-02-12 20:45:20 +0900

    키보드 텍스트뷰 가릴때 위치 조정

--- LocalPub/LocalPub/ViewControllers/ProfileViewController.swift
+++ LocalPub/LocalPub/ViewControllers/ProfileViewController.swift
@@ -15,6 +15,9 @@ class profileViewController: UIViewController, UITextFieldDelegate {
     
     // let listResidenceKorea = [ "Seoul", "Busan", "Incheon" ]
 
+    // to store the current active textfield
+    var activeTextField: UITextField? = nil
+    
     @IBOutlet var navProfile: UINavigationItem!
     
     @IBOutlet weak var birthDateTextField: UITextField!
@@ -47,25 +50,13 @@ class profileViewController: UIViewController, UITextFieldDelegate {
         
         txtResidence.text = myUserDefaults.string(forKey: UserDefault.Residence.toString() )
         
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-                
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        registerForKeyboardNotification()
+//    registerForKeyboardNotification()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
-    
-    @objc
-    func keyboardWillShow(_ sender: Notification) {
-        self.view.frame.origin.y = -100 // Move view 150 points upward
-    }
-
-     @objc
-        func keyboardWillHide(_ sender: Notification) {
-        self.view.frame.origin.y = 0 // Move view to original position
-    }
-
     
     func SetLocalized() {
         
@@ -82,6 +73,56 @@ class profileViewController: UIViewController, UITextFieldDelegate {
         scGender.setTitle( "Female".localized(), forSegmentAt: 1 )
     }
     
+    func addDoneButton() {
+        
+    }
+    func registerForKeyboardNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+
+        guard let info = notification.userInfo, let keyboardFrameValue = info[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue else { return
+        }
+        
+        let keyboardSize = keyboardFrameValue.cgRectValue.size
+
+        var shouldMoveViewUp = false
+
+      // if active text field is not nil
+        if let activeTextField = activeTextField {
+            let bottomOfTextField = activeTextField.convert(activeTextField.bounds, to: self.view).maxY;
+            
+            let topOfKeyboard = self.view.frame.height - keyboardSize.height
+
+            // if the bottom of Textfield is below the top of keyboard, move up
+            if bottomOfTextField > topOfKeyboard {
+              shouldMoveViewUp = true
+            }
+      }
+
+      if(shouldMoveViewUp) {
+        self.view.frame.origin.y = 0 - keyboardSize.height
+      }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        self.view.frame.origin.y = 0
+    }
+    
+    // when user select a textfield, this method will be called
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        // set the activeTextField to the selected textfield
+        self.activeTextField = textField
+    }
+        
+    // when user click 'done' or dismiss the keyboard
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        self.activeTextField = nil
+    }
+    
+    // when user 'return' key dismiss the keyboard
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         self.txtName.resignFirstResponder()
         self.txtResidence.resignFirstResponder()
@@ -91,42 +132,79 @@ class profileViewController: UIViewController, UITextFieldDelegate {
     func createDatePicker() {
         // preferred Style = Wheels
         datePicker.preferredDatePickerStyle = UIDatePickerStyle.wheels
-        
+
         // tool bar
         let toolBar = UIToolbar()
         toolBar.sizeToFit()
-        
+
         // bar button
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(donePressed))
-        toolBar.setItems([doneButton], animated: true)
-        
+        toolBar.setItems([flexSpace, doneButton], animated: true)
+
         // assign tool bar
         birthDateTextField.inputAccessoryView = toolBar
         birthDateTextField.inputView = datePicker
-        
+
         // date picker mode
         datePicker.datePickerMode = .date
     }
-    
+
     @objc func donePressed() {
         let datestr = myUserDefaults.string(forKey: "birthState")
         let dateFormatter = DateFormatter()
-        
+
         dateFormatter.dateStyle = .medium
         dateFormatter.timeStyle = .none
         dateFormatter.dateFormat = "yyyy-MM-dd"
-        
+
         birthDateTextField.text =  dateFormatter.string(from: datePicker.date)
-        
+
         let datetime = dateFormatter.date(from: datestr ?? dateFormatter.string( from: Date() ) )
-        
+
         datePicker.setDate(datetime!, animated: true)
-    
+
         self.view.endEditing(true)
     }
     
+    @IBAction func saveProfile(_ sender: UIButton) {
+
+        SaveUserDefault( key: UserDefault.Name.toString(), value: txtName.text! )
+        
+        SaveUserDefault( key: UserDefault.Residence.toString(), value: txtResidence.text! )
+        
+        SaveUserDefault( key: UserDefault.Birth.toString(), value: birthDateTextField.text! )
+    }
+    
+    
+    @IBAction func selectGender(_ sender: UISegmentedControl) {
+        
+        print( sender.selectedSegmentIndex )
+        
+        SaveUserDefault( key: UserDefault.Gender.toString(), value: sender.selectedSegmentIndex )
+    }
     
     /*
+    @IBAction func selectBirth(_ sender: UIDatePicker) {
+        
+        let dateform = DateFormatter()
+        dateform.dateFormat = "yyyy-MM-dd"
+        let strDate = dateform.string( from:sender.date )
+        print( strDate )
+        
+        SaveUserDefault( key: UserDefault.Birth.toString(), value: strDate )
+        
+    }
+    
+    
+    func setResidence(_ i: Int ) {
+    
+        btnResidence.setTitle( listResidenceKorea[i].localized(), for: .normal)
+        
+        //myUserDefaults.set( i, forKey: "Residence" )
+        
+    }
+     
     func SetBirth() {
         
         let birth = myUserDefaults.string( forKey: UserDefault.Birth.toString() )
@@ -155,49 +233,10 @@ class profileViewController: UIViewController, UITextFieldDelegate {
         
     }
     */
-    
-    @IBAction func saveProfile(_ sender: UIButton) {
-
-        SaveUserDefault( key: UserDefault.Name.toString(), value: txtName.text! )
-        
-        SaveUserDefault( key: UserDefault.Residence.toString(), value: txtResidence.text! )
-        
-        SaveUserDefault( key: UserDefault.Birth.toString(), value: birthDateTextField.text! )
-    }
-    
-    @IBAction func selectGender(_ sender: UISegmentedControl) {
-        
-        print( sender.selectedSegmentIndex )
-        
-        SaveUserDefault( key: UserDefault.Gender.toString(), value: sender.selectedSegmentIndex )
-        
-    }
-    
-    /*
-    @IBAction func selectBirth(_ sender: UIDatePicker) {
-        
-        let dateform = DateFormatter()
-        dateform.dateFormat = "yyyy-MM-dd"
-        let strDate = dateform.string( from:sender.date )
-        print( strDate )
-        
-        SaveUserDefault( key: UserDefault.Birth.toString(), value: strDate )
-        
-    }
-    
-    
-    func setResidence(_ i: Int ) {
-    
-        btnResidence.setTitle( listResidenceKorea[i].localized(), for: .normal)
-        
-        //myUserDefaults.set( i, forKey: "Residence" )
-        
-    }
-    */
-    
-
-    
-    
 }
+
+
+
+
 
 
